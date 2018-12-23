bb9f2dbda78e6b0c78b835384e3694d47e67c11f
datvithanh98 <datvithanh98@gmail.com>
2018-12-23 18:16:23 +0700

    up

--- dhcp6c.py
+++ dhcp6c.py
@@ -1,17 +1,22 @@
 from dhcp6 import *
 from scapy.all import *
 
-if __name__ == "__main__":
-    dhcp6c = DHCPv6_am()
-    print(dhcp6c.duid)
-    sol = DHCP6_Solicit()
-    opreq = DHCP6OptOptReq()
-    et= DHCP6OptElapsedTime()
-    cid = DHCP6OptClientId()
-    iana = DHCP6OptIA_NA()
-    optiana = DHCP6OptIAAddress()
+# if __name__ == "__main__":
+#     dhcp6c = DHCPv6_am()
+#     sol = DHCP6_Solicit()
+#     opreq = DHCP6OptOptReq()
+#     et= DHCP6OptElapsedTime()
+#     cid = DHCP6OptClientId()
+#     iana = DHCP6OptIA_NA()
+#     optiana = DHCP6OptIAAddress()
 
-    l3 = Ether (dst="ff:ff:ff:ff:ff:ff", src = RandMAC())
-    l2 = IPv6(dst=dstt, src=srcc)
-    pkt = l2/l3/sol/cid/opreq/et/iana
-    sendp(pkt, iface='eth0')
+#     l3 = Ether (dst="ff:ff:ff:ff:ff:ff", src = RandMAC())
+#     l2 = IPv6(dst=dstt, src=srcc)
+#     pkt = l2/l3/sol/cid/opreq/et/iana
+#     sendp(pkt, iface='eth0')
+
+l3 = Ether(dst="ff:ff:ff:ff:ff:ff",src=RandMAC())
+l2 = IPv6(dst="ff02::1:2", src="::0")
+l1 = UDP(dport=547)
+sol = DHCP6_Solicit()
+send(l3/l2/l1/sol)

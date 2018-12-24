33ee4472e3a7d43afe358fc7a49fb4b5fa7b387c
datvithanh98 <datvithanh98@gmail.com>
2018-12-24 09:28:40 +0700

    send success packet

--- dhcp6c.py
+++ dhcp6c.py
@@ -15,8 +15,25 @@ from scapy.all import *
 #     pkt = l2/l3/sol/cid/opreq/et/iana
 #     sendp(pkt, iface='eth0')
 
-l3 = Ether(dst="ff:ff:ff:ff:ff:ff",src=RandMAC())
-l2 = IPv6(dst="ff02::1:2", src="::0")
-l1 = UDP(dport=547)
+# l3 = Ether(dst="ff:ff:ff:ff:ff:ff",src=RandMAC())
+# l2 = IPv6(dst="ff02::1:2", src="::0")
+# l1 = UDP(dport=547)
+# sol = DHCP6_Solicit()
+# send(l3/l2/l1/sol)
+
 sol = DHCP6_Solicit()
-send(l3/l2/l1/sol)
+# opreq = DHCP6OptOptReq()
+# et = DHCP6OptElapsedTime()
+cid = DHCP6OptClientId()
+# iana = DHCP6OptIA_NA()
+# optiana = DHCP6OptIAAddress()
+
+ether = Ether(dst="ff:ff:ff:ff:ff:ff", src="8c:85:90:39:f5:17")
+ip = IPv6(dst="ff02::1:2", src="::0")
+udp = UDP(dport=547,sport=546)
+# addr = raw_input("Give Option Address: ")
+# optiana.addr = str(RandIP6(addr))
+pkt = ether/ip/udp/sol/cid#/opreq/et/iana
+sendp(pkt)#, iface='en0')
+
+# eth.addr == ff:ff:ff:ff:ff:ff
